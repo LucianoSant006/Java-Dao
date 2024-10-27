c9cdef63b5d996571e3eee8575d640ffc57a94be
LucianoSant006 <santiagoluciano383@gmail.com>
2024-10-27 16:53:22 -0300

    findById implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -4,15 +4,16 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
+import java.util.Date;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+         SellerDao sellerDao = DaoFactory.createSellerDao();
+         Seller seller = sellerDao.findById(3);
+         System.out.println(seller);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        System.out.println(seller);
 
     }
 }
